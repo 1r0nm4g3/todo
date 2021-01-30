c6a5181c61d745026fb8e0d8861fd6f0033b4c67
Michael Ingram <1r0nm4g3@gmail.com>
2021-01-30 13:27:16 -0500

    testing if db is the issue

--- config/db.js
+++ config/db.js
@@ -1,29 +1,29 @@
 const mongoose = require('mongoose');
 const config = require('config');
 const e = require('express');
-// let db = config.get('mongoURI');
+// // let db = config.get('mongoURI');
 
-if(process.env.NODE_ENV === 'production'){
-    const db = process.env.MONGO_URI
-}else{
-    const db = config.get('mongoURI')
-}
+// if(process.env.NODE_ENV === 'production'){
+//     const db = process.env.MONGO_URI
+// }else{
+//     const db = config.get('mongoURI')
+// }
 
 
-const connectDB = async () => {
-    try{
-        await mongoose.connect(db, {
-            useCreateIndex: true,
-            useFindAndModify: false,
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
+// const connectDB = async () => {
+//     try{
+//         await mongoose.connect(db, {
+//             useCreateIndex: true,
+//             useFindAndModify: false,
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true
+//         })
         
-        console.log('MongoDB Connected')
-    } catch (err) {
-        console.error(err.message);
-        process.exit(1);
-    }
-}
+//         console.log('MongoDB Connected')
+//     } catch (err) {
+//         console.error(err.message);
+//         process.exit(1);
+//     }
+// }
 
-module.exports = connectDB;
+// module.exports = connectDB;
